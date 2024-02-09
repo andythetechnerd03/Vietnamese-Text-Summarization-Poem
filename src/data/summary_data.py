d469718f1af1f5abc0e09f959e6fa50247c1ca58
pphuc25 <phanphuc1100@gmail.com>
2024-02-09 23:46:32 +0700

    chorse: update file summary data

--- src/data/summary_data.py
+++ src/data/summary_data.py
@@ -7,6 +7,8 @@ from transformers import (
 )
 from tqdm import tqdm
 import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 
 
 def parse_arguments():
@@ -16,6 +18,10 @@ def parse_arguments():
     parser.add_argument('--input_path', type=str, default='/home/pphuc/Coding/Project/Vietnamese-Poem-Summarization/data/processed/data_split_sentence.json',
                         help='')
     parser.add_argument('--output_path', type=str, default='/home/pphuc/Coding/Project/Vietnamese-Poem-Summarization/data/processed/data_fpt_summary.json',
+                        help='')
+    parser.add_argument('--num_proc', type=int, default=2,
+                        help='')
+    parser.add_argument('--num_worker', type=int, default=8,
                         help='')
     args = parser.parse_args()
     return args
@@ -31,27 +37,55 @@ def save_json(path, file_save):
         return json.dump(file_save, f)
 
 
+class CustomImageDataset(Dataset):
+    def __init__(self, datasets):
+        self.datasets = datasets
+
+    def __len__(self):
+        return len(self.datasets)
+
+    def __getitem__(self, idx):
+        text = self.datasets[idx]["input_ids"]
+        return text
+
+template = [
+    {
+        "role": "system",
+        "content": "Bạn là một nhà kể chuyện chuyên nghiệp, nhiệm vụ của bạn là hãy kể 1 câu chuyện từ một bài thơ"
+    }
+]
+
+
 def main():
     args = parse_arguments()
-    # data = load_json(args.input_path)
     data = load_dataset("json", args.input_path, split="train")
 
     model = AutoModelForCausalLM.from_pretrained(
         args.model,
-        device_map="auto", 
+        device_map="auto",
         attn_implementation="flash_attention_2",
         torch_dtype=torch.bfloat16
     )
     tokenizer = AutoTokenizer.from_pretrained(args.model, device_map="auto")
 
-    final_data = []
-    for data_th in tqdm(data):
-        conversation = [{"role": "system",
-                        "content": "Bạn là một nhà kể chuyện chuyên nghiệp, nhiệm vụ của bạn là hãy kể 1 câu chuyện từ một bài thơ"}]
 
-        conversation.append({"role": "user", "content": data_th})
-        # input_ids = tokenizer.apply_chat_template(conversation, return_tensors="pt").to(model.device)
-        input_ids = tokenizer.apply_chat_template(conversation, return_tensors="pt")
+    def tokenizer_data_fn(data):
+        poem = data["poem"]
+        data["input_ids"] = tokenizer.apply_chat_template(template + [poem], return_tensors="pt")
+        return data
+    
+    tokenized_data = data.map(tokenizer_data_fn, num_proc=args.num_proc)
+
+
+    infer_data = CustomImageDataset(datasets=tokenized_data)
+    dataloader = DataLoader(infer_data, batch_size=args.batch_size, num_workers=args.num_worker)
+
+
+    final_data = []
+
+    for batch in dataloader:
+        input_ids = tokenizer.apply_chat_template(batch, return_tensors="pt")
+
         out_ids = model.generate(
             input_ids=input_ids,
             max_new_tokens=512,
@@ -61,17 +95,39 @@ def main():
             temperature=0.1,
             repetition_penalty=1.05,
         )
-        output = tokenizer.batch_decode(
-            out_ids[:, input_ids.size(1):], skip_special_tokens=True)[0].strip()
 
-        output_formated = {
-            "input": data_th,
-            "output": output
-        }
+        print(out_ids)
+        print(out_ids.shape)
+        break
 
-        final_data.append(output_formated)
+    # for data_th in tqdm(data):
+    #     conversation = [{"role": "system",
+    #                     "content": "Bạn là một nhà kể chuyện chuyên nghiệp, nhiệm vụ của bạn là hãy kể 1 câu chuyện từ một bài thơ"}]
 
-    save_json(args.ouput_path, final_data)
+    #     conversation.append({"role": "user", "content": data_th})
+    #     # input_ids = tokenizer.apply_chat_template(conversation, return_tensors="pt").to(model.device)
+    #     input_ids = tokenizer.apply_chat_template(
+    #         conversation, return_tensors="pt")
+    #     out_ids = model.generate(
+    #         input_ids=input_ids,
+    #         max_new_tokens=512,
+    #         do_sample=True,
+    #         top_p=0.95,
+    #         top_k=40,
+    #         temperature=0.1,
+    #         repetition_penalty=1.05,
+    #     )
+    #     output = tokenizer.batch_decode(
+    #         out_ids[:, input_ids.size(1):], skip_special_tokens=True)[0].strip()
+
+    #     output_formated = {
+    #         "input": data_th,
+    #         "output": output
+    #     }
+
+    #     final_data.append(output_formated)
+
+    # save_json(args.ouput_path, final_data)
 
 
 if __name__ == "__main__":
