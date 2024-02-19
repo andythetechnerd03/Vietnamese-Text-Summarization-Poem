95879bac4dd576c7c8d4bc511109918962aed8ea
pphuc25 <phanphuc1100@gmail.com>
2024-02-20 00:33:58 +0700

    chorse: update install_packages

---
+++ install_packages.sh
@@ -0,0 +1,12 @@
+pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https://download.pytorch.org/whl/cu121
+pip install kaleido cohere openai
+pip install datasets -U
+pip install -i https://pypi.org/simple/ bitsandbytes
+pip install transformers
+pip install mpi4py
+
+git clone -q https://github.com/OpenAccess-AI-Collective/axolotl
+%cd axolotl
+
+pip install packaging huggingface_hub torch==2.1.2 --progress-bar off
+pip install -e '.[flash-attn,deepspeed]'
